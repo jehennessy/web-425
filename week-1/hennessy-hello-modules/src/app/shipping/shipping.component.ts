d4241b08caae2c379fcfdc6c2c396205a6af6374
Jordan Hennessy <jorrdygirl@me.com>
2019-07-14 20:51:26 -0500

    Assignment 1.5

---
+++ week-1/hennessy-hello-modules/src/app/shipping/shipping.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping',
+  template: `
+    <p>
+      Hello this is the ShippingComponent
+    </p>
+  `,
+  styles: []
+})
+export class ShippingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
